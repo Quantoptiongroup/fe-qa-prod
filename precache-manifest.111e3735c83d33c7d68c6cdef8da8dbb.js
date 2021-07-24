27b1547b30ffd423fc35faebbd8757ccb50a451f
qog-iris <iris@quantoptiongroup.com>
2021-07-24 14:06:29 -0700

    Updates

---
+++ precache-manifest.111e3735c83d33c7d68c6cdef8da8dbb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10c571d922e6053352a228d7030d2e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e8380e3dd1641bd80a8",
+    "url": "/static/css/2.b6642dd1.chunk.css"
+  },
+  {
+    "revision": "2069403828743ce6d47c",
+    "url": "/static/css/main.a06eaa72.chunk.css"
+  },
+  {
+    "revision": "3e8380e3dd1641bd80a8",
+    "url": "/static/js/2.8f81795a.chunk.js"
+  },
+  {
+    "revision": "68bd64590edaddbc2c76924e3401a365",
+    "url": "/static/js/2.8f81795a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2069403828743ce6d47c",
+    "url": "/static/js/main.6520d1c9.chunk.js"
+  },
+  {
+    "revision": "9a2141e4994de110554b",
+    "url": "/static/js/runtime-main.aedd8f15.js"
+  }
+]);
