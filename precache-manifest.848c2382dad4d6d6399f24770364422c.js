f243c77fef5a000836be1729372b81ce35f346fd
qogadmin <admin@quantoptiongroup.com>
2022-05-28 12:33:17 +0000

    Updates

--- precache-manifest.848c2382dad4d6d6399f24770364422c.js
+++ precache-manifest.848c2382dad4d6d6399f24770364422c.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ce1492b488676de23fbfd294d22ca6b4",
+    "revision": "4bc39dc4bf11e6aaab061d850caaae26",
     "url": "/index.html"
   },
   {
-    "revision": "cc405e2237b0abf77d34",
+    "revision": "2bc9a358b226b0fcb4c6",
     "url": "/static/css/2.f01b737b.chunk.css"
   },
   {
-    "revision": "ee95ac519518a8b85b70",
+    "revision": "9a0761650565f270c5c9",
     "url": "/static/css/main.9c6b941a.chunk.css"
   },
   {
-    "revision": "cc405e2237b0abf77d34",
-    "url": "/static/js/2.d00faab6.chunk.js"
+    "revision": "2bc9a358b226b0fcb4c6",
+    "url": "/static/js/2.5a09a977.chunk.js"
   },
   {
     "revision": "cf41db16f3e4c136cf7ada02c6d92669",
-    "url": "/static/js/2.d00faab6.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.5a09a977.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ee95ac519518a8b85b70",
-    "url": "/static/js/main.7241d18a.chunk.js"
+    "revision": "9a0761650565f270c5c9",
+    "url": "/static/js/main.5cd92054.chunk.js"
   },
   {
     "revision": "9a2141e4994de110554b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.96b96d137572c30fb44c5d87eda4f707.js"
+  "/precache-manifest.848c2382dad4d6d6399f24770364422c.js"
 );
 
 self.addEventListener('message', (event) => {
