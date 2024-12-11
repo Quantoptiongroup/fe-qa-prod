9afa86564752553ed7f7c7b254bcc48fd7a1fd47
Crab <crab@quantoptiongroup.com>
2024-12-10 17:02:35 -0800

    Updates

---
+++ precache-manifest.d6762c708b64265e0ba683382d4c42d0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5115f42ae7e0072d8ecdb427cd64f06c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c13a13d2bee3b5d76f8",
+    "url": "/static/css/2.f01b737b.chunk.css"
+  },
+  {
+    "revision": "63187e4a22d409b13502",
+    "url": "/static/css/main.def8276c.chunk.css"
+  },
+  {
+    "revision": "1c13a13d2bee3b5d76f8",
+    "url": "/static/js/2.b15eb658.chunk.js"
+  },
+  {
+    "revision": "2fa94dee89d2b9464f32ea23b5169282",
+    "url": "/static/js/2.b15eb658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63187e4a22d409b13502",
+    "url": "/static/js/main.31a6a277.chunk.js"
+  },
+  {
+    "revision": "9a2141e4994de110554b",
+    "url": "/static/js/runtime-main.aedd8f15.js"
+  },
+  {
+    "revision": "4355078f6a948669ba3cf70045ac0a4b",
+    "url": "/static/media/alert.4355078f.mp3"
+  },
+  {
+    "revision": "5ce15f800623e6e13ede276ea73ed067",
+    "url": "/static/media/logo.5ce15f80.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0c28fa961d16f28a41d436f3be02cf56.js"
+  "/precache-manifest.d6762c708b64265e0ba683382d4c42d0.js"
 );
 
 self.addEventListener('message', (event) => {
